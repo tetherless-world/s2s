c569c0684df43a9f111396e139f648b0e6360db0
zednis <lokipatera@gmail.com>
2014-11-11 14:38:14 -0700

    Fixes #9 Refactored to use local test RDF file instead of HTTP URL to aquarius.  Commented out code that creates RippleSource and ignoring all related tests.  I think we will have to mock the RippleSource and related service objects.

--- server/src/test/java/edu/rpi/tw/sesf/s2s/web/service/impl/FacetOntologyServiceEngineTest.java
+++ server/src/test/java/edu/rpi/tw/sesf/s2s/web/service/impl/FacetOntologyServiceEngineTest.java
@@ -1,39 +1,28 @@
-/**
- * 
- */
 package edu.rpi.tw.sesf.s2s.web.service.impl;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.junit.Assume;
-
-import org.junit.Ignore;
-import org.springframework.mock.web.MockHttpServletResponse;
-
-import net.fortytwo.ripple.RippleException;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 import edu.rpi.tw.sesf.s2s.data.JenaPelletSource;
 import edu.rpi.tw.sesf.s2s.data.RippleSource;
-import edu.rpi.tw.sesf.s2s.exception.IncompatibleDataSourceException;
-import edu.rpi.tw.sesf.s2s.exception.InvalidLinkedDataIdentifierException;
-import edu.rpi.tw.sesf.s2s.exception.UnregisteredInstanceException;
 import edu.rpi.tw.sesf.s2s.impl.lod.SearchServiceFromRipple;
 import edu.rpi.tw.sesf.s2s.impl.sparql.SearchServiceFromSparql;
 import edu.rpi.tw.sesf.s2s.utils.RippleQueryEngineSingleton;
-import junit.framework.TestCase;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
 
-/**
- * @author rozele
- *
- */
-public class FacetOntologyServiceEngineTest extends TestCase {
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+
+public class FacetOntologyServiceEngineTest {
 
 	private static final String TEST_SERVICE_URI = "http://aquarius.tw.rpi.edu/s2s/BCO-DMO/1.0/s2s.ttl#FacetOntologyService";
 	private static final String TEST_SERVICE_QUERY = "http://aquarius.tw.rpi.edu/s2s/BCO-DMO/1.0/s2s.ttl#PeopleFacet";
@@ -42,61 +31,31 @@ public class FacetOntologyServiceEngineTest extends TestCase {
 	private static final String TEST_SERVICE_QUERY_RESULT = "http://escience.rpi.edu/ontology/BCO-DMO/bcodmo/3/0/Person_1";
 	private static final String TEST_FORMAT = "TURTLE";
 	
-	private SearchServiceFromSparql _sparqlService;
-	private SearchServiceFromRipple _linkedService;
+	private static SearchServiceFromSparql _sparqlService;
+	private static SearchServiceFromRipple _linkedService;
 
-    /**
-	 * @param name
-	 * @throws UnregisteredInstanceException 
-	 * @throws InvalidLinkedDataIdentifierException 
-	 * @throws RippleException 
-	 * @throws IncompatibleDataSourceException 
-	 */
-	public FacetOntologyServiceEngineTest(String name) throws UnregisteredInstanceException, InvalidLinkedDataIdentifierException, RippleException, IncompatibleDataSourceException {
-		super(name);
-		JenaPelletSource source = new JenaPelletSource("test",TEST_SERVICE_URI, TEST_FORMAT);
-		RippleSource rsource = new RippleSource("test",RippleQueryEngineSingleton.getInstance());
+	@BeforeClass
+	public static void setUp() throws Exception {
+
+		Model m = ModelFactory.createDefaultModel();
+		URL testFile = FacetOntologyServiceEngineTest.class.getResource("/rdf/BCO-DMO/1.0/s2s.ttl");
+		m.read(testFile.openStream(), null, TEST_FORMAT);
+
+		JenaPelletSource source = new JenaPelletSource("test", m);
 		_sparqlService = new SearchServiceFromSparql(TEST_SERVICE_URI, source);
-		_linkedService = new SearchServiceFromRipple(TEST_SERVICE_URI, rsource);
-        FacetOntologyServiceEngine _sparqlEngine = new FacetOntologyServiceEngine(_sparqlService);
-        FacetOntologyServiceEngine _linkedEngine = new FacetOntologyServiceEngine(_linkedService);
+		FacetOntologyServiceEngine _sparqlEngine = new FacetOntologyServiceEngine(_sparqlService);
 		_sparqlService.setWebServiceEngine(_sparqlEngine);
-		_linkedService.setWebServiceEngine(_linkedEngine);
+
+		// TODO mock the RippleSource?
+		//RippleSource rsource = new RippleSource("test",RippleQueryEngineSingleton.getInstance());
+		//_linkedService = new SearchServiceFromRipple(TEST_SERVICE_URI, rsource);
+		//FacetOntologyServiceEngine _linkedEngine = new FacetOntologyServiceEngine(_linkedService);
+		//_linkedService.setWebServiceEngine(_linkedEngine);
 	}
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
-		
-		super.setUp();
-	}
-
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
-
-	public void beforeTest() {
-		try {
-			Configuration config = new PropertiesConfiguration("src/main/resources/s2s.properties");
-			Assume.assumeTrue(config.containsKey("test.rpi") && config.getBoolean("test.rpi"));
-		} catch (ConfigurationException e) {
-			Assume.assumeTrue(false);
-		}
-	}
-	
+	@Test
 	public void testRunQuerySparql() throws IOException, JSONException {
-		try {
-			Configuration config = new PropertiesConfiguration("src/main/resources/s2s.properties");
-			if (!config.containsKey("test.rpi") || !config.getBoolean("test.rpi")) {
-				return;
-			}
-		} catch (ConfigurationException e) {
-			return;
-		}
+
 		MockHttpServletResponse response = new MockHttpServletResponse();
 		Map<String,String> inputs = new HashMap<>();
 		inputs.put(TEST_SERVICE_INPUT, TEST_SERVICE_INPUT_VALUE);
@@ -113,18 +72,13 @@ public class FacetOntologyServiceEngineTest extends TestCase {
 				}
 			}
 		}
+
 		assertTrue(check);
 	}
-	
+
+	@Ignore
+	@Test
 	public void testRunQueryLod() throws IOException, JSONException {
-		try {
-			Configuration config = new PropertiesConfiguration("src/main/resources/s2s.properties");
-			if (!config.containsKey("test.rpi") || !config.getBoolean("test.rpi")) {
-				return;
-			}
-		} catch (ConfigurationException e) {
-			return;
-		}
 		MockHttpServletResponse response = new MockHttpServletResponse();
 		Map<String,String> inputs = new HashMap<>();
 		inputs.put(TEST_SERVICE_INPUT, TEST_SERVICE_INPUT_VALUE);
@@ -143,20 +97,25 @@ public class FacetOntologyServiceEngineTest extends TestCase {
 		}
 		assertTrue(check);
 	}
-	
+
+	@Test
 	public void testGetSearchParametersSparql() {
 		assertTrue(_sparqlService.getWebServiceEngine().getInputs().contains(TEST_SERVICE_INPUT));
 	}
 
+	@Test
 	public void testGetQueryInterfacesSparql() {
 		assertTrue(_sparqlService.getWebServiceEngine().getInterfaces().contains(TEST_SERVICE_INPUT));
 	}
 
+	@Ignore
+	@Test
 	public void testGetSearchParametersLod() {
 		assertTrue(_linkedService.getWebServiceEngine().getInputs().contains(TEST_SERVICE_INPUT));
 	}
 
     @Ignore
+	@Test
 	public void testGetQueryInterfacesLod() {
 		assertTrue(_linkedService.getWebServiceEngine().getInterfaces().contains(TEST_SERVICE_INPUT));
 	}
